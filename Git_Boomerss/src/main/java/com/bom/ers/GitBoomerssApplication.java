752d5d447c1cc8e033d6fc1a6c9316a9cdde59e9
Taiho <Taiho@DESKTOP-3JFTEL0>
2025-04-15 14:56:27 +0900

    Hello!

---
+++ Git_Boomerss/src/main/java/com/bom/ers/GitBoomerssApplication.java
@@ -0,0 +1,13 @@
+package com.bom.ers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBoomerssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBoomerssApplication.class, args);
+	}
+
+}
