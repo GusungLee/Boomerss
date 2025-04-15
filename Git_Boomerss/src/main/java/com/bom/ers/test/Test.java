f88e7f818cc4df7b4c30bb3aad8c3469d9b7200e
Taiho <Taiho@DESKTOP-3JFTEL0>
2025-04-15 15:29:49 +0900

    테스트!

---
+++ Git_Boomerss/src/main/java/com/bom/ers/test/Test.java
@@ -0,0 +1,12 @@
+package com.bom.ers.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("안녕하세요 테스트하러왔어요!");
+
+	}
+
+}
