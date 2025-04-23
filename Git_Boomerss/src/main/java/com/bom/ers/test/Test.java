9f6dc14254d2b6809a59ea0d15e5fc98a811c760
Taiho <Taiho@DESKTOP-A7RMNK6>
2025-04-23 09:26:33 +0900

    1848

--- Git_Boomerss/src/main/java/com/bom/ers/test/Test.java
+++ Git_Boomerss/src/main/java/com/bom/ers/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		
 		System.out.println("안녕하세요 테스트하러왔어요!");
 		System.out.println("안녕하세요 dev브런치만 확인할거에요!!");
-		System.out.println("ㅇㅅㅇ");
+		System.out.println("ㅇㅅㅇ51251515");
 	}
 
 }
